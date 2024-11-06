a1009399b91a8f2f1bd52c6dcec8a481dceb8311
sidoniastanton <sidonia.stanton@maine.edu>
2024-11-06 10:09:43 -0500

    commit 1
    countEvens and countLarger done

--- main.py
+++ main.py
@@ -13,3 +13,21 @@ def countEvens(aList):
 x = countEvens([2,5,7, 8,10])
 print(x)
 print(countEvens([10,5,20]))
+
+
+numArray = []
+x = ()
+listSize = len(numArray)
+def countLarger(numArray):
+    numCounter = 0
+    for i in numArray:
+        if i > x:
+            numCounter += 1
+    return numCounter
+
+x = 4 
+numArray = [2, 6, 34, 60]
+numLarger = countLarger(numArray)
+print(numLarger)
+
+
